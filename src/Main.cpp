9f05cdbdd50b5620dc93898575cc789651acd2ea
Judah Jeevan Legy <judahlegy@gmail.com>
2024-06-17 17:50:22 +0530

    Now Using SKSE Menu Framework for config Modification

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,20 +1,20 @@
-#include "DataHandler.h"
-#include "Events.h"
-#include "logging.h"
+#include "Main.h"
 
 SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
-	auto startSPL = std::chrono::high_resolution_clock::now();
+	std::chrono::steady_clock::time_point startSPL = std::chrono::high_resolution_clock::now();
 	AP::InitializeLogging();
 	SKSE::Init(a_skse);
 	DataHandler* d = DataHandler::GetSingleton();
+
 	d->LoadJson();
 
 	if (d->_InfinitePlayerAmmo || d->_InfiniteTeammateAmmo) {
 		APEventProcessor::RegisterEvent();
 	}
 	SKSEEvent::InitializeMessaging();
-	auto nanosecondsTakenForSPL = std::chrono::duration(std::chrono::high_resolution_clock::now() - startSPL);
+	UI::Register();
+	std::chrono::nanoseconds nanosecondsTakenForSPL = std::chrono::duration(std::chrono::high_resolution_clock::now() - startSPL);
 
 	logger::info("Time Taken in SKSEPluginLoad(const SKSE::LoadInterface* a_skse) totally is {} nanoseconds or {} microseconds or {} milliseconds or {} seconds or {} minutes", nanosecondsTakenForSPL.count(),
 		std::chrono::duration_cast<std::chrono::microseconds>(nanosecondsTakenForSPL).count(), std::chrono::duration_cast<std::chrono::milliseconds>(nanosecondsTakenForSPL).count(),
